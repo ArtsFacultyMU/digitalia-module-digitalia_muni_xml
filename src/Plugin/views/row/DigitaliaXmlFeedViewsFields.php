ef8c4f012c87741fdf1bcf34da8d15d735ebdbdf
bstrakosova <strakosova@phil.muni.cz>
2024-01-09 12:23:51 +0100

    Added module Digitalia MUNI XML

---
+++ src/Plugin/views/row/DigitaliaXmlFeedViewsFields.php
@@ -0,0 +1,333 @@
+<?php
+
+namespace Drupal\digitalia_muni_xml\Plugin\views\row;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\row\RowPluginBase;
+use Drupal\Core\Url;
+use Drupal\xmlfeedviews\Plugin\views\row\XmlFeedViewsFields;
+
+
+/**
+ * Renders an XML FEED VIEWS item based on fields.
+ *
+ * @ViewsRow(
+ *   id = "digitaliaxmlfeedviews_fields",
+ *   title = @Translation("Digitalia XML Feed Views fields"),
+ *   help = @Translation("Display fields as XML items."),
+ *   theme = "xmlfeedviews_row",
+ *   display_types = {"feed"}
+ * )
+ */
+class DigitaliaXmlFeedViewsFields extends XmlFeedViewsFields {
+
+  /**
+   * Render.
+   *
+   * @param object $row
+   *   Row object.
+   *
+   * @return array|string
+   *   Returns array or string.
+   */
+  public function render($row) {
+    static $row_index;
+    if (!isset($row_index)) {
+      $row_index = 0;
+    }
+
+    $item = new \stdClass();
+    $item->body_before = isset($this->options['xmlfeedviews_body_before']) ? $this->options['xmlfeedviews_body_before'] : NULL;
+    $item->body = $this->getBodyField($row_index, $this->options['xmlfeedviews_body']);
+    $item->body_after = isset($this->options['xmlfeedviews_body_after']) ? $this->options['xmlfeedviews_body_after'] : NULL;
+
+    $row_index++;
+
+    $build = [
+      '#theme' => $this->themeFunctions(),
+      '#view' => $this->view,
+      '#options' => $this->options,
+      '#row' => $item,
+      '#field_alias' => isset($this->field_alias) ? $this->field_alias : '',
+    ];
+
+    $body = $item->body;
+    $article_id = trim(explode('</id>', explode('<id type="internal" advice="ignore">', $body)[1])[0]);
+    $article = \Drupal\node\Entity\Node::load($article_id);
+ 
+    // version
+    if (str_contains($body, '###version###') and str_contains($body, '###advice###')) {
+      $advice = 'ignore';
+      $version = $article->get('field_ojs_version')->getValue()[0]['value'];
+      if (empty($version)) {
+        $version = 1;
+      }
+      $ojs_source = $article->get('field_ojs_source')->getValue();
+      if (!empty($ojs_source) and $ojs_source[0]['value']) {
+        $advice = 'update';
+        $version = max(2, $version);
+      }
+
+      $body = str_replace('###version###', $version, $body);
+      $body = str_replace('###advice###', $advice, $body);
+
+    } 
+
+    $serial_languages = $this->getSerialLanguages($article);
+    $locales = $this->getLocales();
+
+    $titles = $this->getTitles($article, $locales, $serial_languages);
+    $body = str_replace('###titles###', $titles, $body);
+
+    $abstracts = $this->getAbstracts($article, $locales, $serial_languages);
+    $body = str_replace('###abstracts###', $abstracts, $body);
+
+    $keywords = $this->getKeywords($article, $locales, $serial_languages);
+    $body = str_replace('###keywords###', $keywords, $body);
+
+    $authors = $this->getAuthors($article, $serial_languages);
+    $body = str_replace('###authors###', $authors, $body);
+
+    $section_field = $article->get('field_section')->getValue();
+    if (!empty($section_field) and $section_id=$section_field[0]['target_id']) {
+      $section = \Drupal\taxonomy\Entity\Term::load($section_id)->getName(); 
+      $body = str_replace('###section###', $section, $body);
+    } else {
+      $body = str_replace('###section###', 'Hidden', $body);
+    }
+    $body = str_replace('###counter###', $row_index, $body);
+
+    $pdf = $this->getPdf($article);
+    $pdf_locale = $this->getLocale($article, $locales);
+    $body = str_replace('###article_galley###', $pdf, $body);
+    $body = str_replace('###pdf_locale###', $pdf_locale, $body);
+
+    $body = str_replace('###license_start###', '<licenseUrl>', $body);
+    $body = str_replace('###license_end###', '</licenseUrl>', $body);
+
+    $body = str_replace('###citations_start###', '<citations>', $body);
+    $body = str_replace('###citations_end###', '</citations>', $body);
+    $body = str_replace('###citation_start###', '<citation>', $body);
+    $body = str_replace('###citation_end###', '</citation>', $body);
+
+    $author_id = $this->getAuthorID($article);
+    $body = str_replace('###primary_contact###', $author_id, $body);
+
+    //dpm(htmlspecialchars($body));
+    $item->body = $body;
+    $item->body_before = str_replace('{{ nid }}', $article_id, $item->body_before); 
+
+    return $build;
+  }
+
+  public function getLocales() {
+    $locales = [];
+    if (($handle = fopen("/var/www/html/drupal/web/modules/custom/digitalia_muni_xml/localization.csv", "r")) !== FALSE) {
+      while ($line = fgetcsv($handle)) {
+        $locales[$line[1]] = $line[2];
+      }
+      fclose($handle);
+      return $locales;
+    }
+    return [];
+  }
+
+  public function getKeywords($article, $locales, $serial_languages) {
+    $keywords = $article->get('field_keywords')->getValue();
+
+    if (empty($keywords)) {
+      return '';
+    }
+
+    $result = [];
+    $result_xml = '';
+
+    foreach ($serial_languages as $lang) {
+      $result[$lang] = '';
+    }
+    foreach ($keywords as $kw) {
+      $lang = $locales[$kw['rel_type']];
+      $tid = $kw['target_id'];
+      $term = \Drupal\taxonomy\Entity\Term::load($tid);
+
+      $result[$lang] = $result[$lang].'<keyword>'.$this->clean($term->getName()).'</keyword>';
+    }
+
+    foreach ($result as $lang => $value) {
+      if ($value != '') {
+        $result_xml = $result_xml.'<keywords locale="'.$lang.'">'.$value.'</keywords>'; 
+      }
+    }
+    return $result_xml;
+  }
+
+  public function getAbstracts($article, $locales, $serial_languages) {
+    $abstracts = $article->get('field_description')->getValue();
+    if (empty($abstracts)) {
+      return '';
+    }
+
+    $result_xml = '';
+    $result = [];
+    foreach ($serial_languages as $lang) {
+      $result[$lang] = '';
+    }
+    
+    foreach ($abstracts as $a) {
+      $lang = $locales[$a['second']];
+      $result[$lang] = $a['first'];
+    }
+
+    foreach ($result as $lang => $a) {
+      if ($a != '') {
+        $result_xml = $result_xml.'<abstract locale="'.$lang.'">'.$this->clean($a).'</abstract>';
+      }
+    }
+    return $result_xml;
+  }
+
+  public function getLocale($article, $locales) {
+    $lang = $article->get('field_language')->getValue();
+    if (!empty($lang)) {
+      $tid = $lang[0]['target_id'];
+      $term = \Drupal\taxonomy\Entity\Term::load($tid);
+      $code = $term->get('field_code')->getValue()[0]['value']; 
+      return $locales[$code];
+    }
+    return '';
+  }
+
+  public function getPdf($article) {
+    $media_array = \Drupal::entityTypeManager()->getStorage('media')->loadByProperties(['field_media_of' => $article->id(), 'bundle' => 'document']);
+    if (empty($media_array)) {
+      return '';
+    }
+    $media = reset($media_array);
+    $fid = $media->get('field_media_document')->getValue()[0]['target_id'];
+    $result = '<id type="internal" advice="ignore">'.$fid.'</id><name locale="###pdf_locale###">PDF</name><seq>0</seq><remote src="';
+    $file = \Drupal\file\Entity\File::load($fid);
+    $url = 'https://digilib.phil.muni.cz'.$file->createFileUrl();
+    $result = $result.$url.'" />';
+    return $result;
+  }
+
+  public function getAuthorID($article) {
+    $authors = $article->get('field_author')->getValue();
+    if (empty($authors)) {
+      return '1';
+    }
+    $first = reset($authors);
+    $author = \Drupal\node\Entity\Node::load($first['target_id']);
+    if (!$author->get('field_author_id')->getValue()) {
+      return '1';
+    }
+    return $author->get('field_author_id')->getValue()[0]['target_id'];
+  }
+
+  public function getAuthors($article, $serial_languages) {
+    $authors = $article->get('field_author')->getValue();
+    $result = '';
+    if (empty($authors)) {
+      $issue_id = $article->get('field_member_of')->getValue()[0]['target_id'];
+      $issue = \Drupal\node\Entity\Node::load($issue_id);
+      if ($volume_id = $issue->get('field_member_of')->getValue()[0]['target_id']) {
+        $volume = \Drupal\node\Entity\Node::load($volume_id);
+        if ($serial_id = $volume->get('field_member_of')->getValue()[0]['target_id']) {
+          $serial = \Drupal\node\Entity\Node::load($serial_id);
+          $serial_title = $serial->getTitle();
+          $result = $result.'<author include_in_browse="true" user_group_ref="Author" seq="1" id="'.$serial_id.'">';
+          foreach ($serial_languages as $lang) {
+            $result = $result.'<givenname locale="'.$lang.'">Journal</givenname><familyname locale="'.$lang.'">'.$serial_title.'</familyname>';
+          }
+          $result = $result.'<email>email@journals.phil.muni.cz</email></author>';
+        }
+      }
+      return $result;
+    }
+
+
+    foreach ($authors as $key => $item) {
+      $author = \Drupal\node\Entity\Node::load($item['target_id']);
+      if ($author !== NULL) {
+        $name = $author->get('field_name_structured')->getValue();
+        $author_id = $author->get('field_author_id')->getValue()[0]['target_id'];
+        $given = $name[0]['given'];
+        $family = $name[0]['family'];
+        $result = $result.'<author include_in_browse="true" user_group_ref="Author" seq="'.($key+1).'" id="'.$author_id.'">';
+
+        foreach ($serial_languages as $lang) {
+          $result = $result.'<givenname locale="'.$lang.'">'.$given.'</givenname>';
+        }
+
+        foreach ($serial_languages as $lang) {
+          $result = $result.'<familyname locale="'.$lang.'">'.$family.'</familyname>';
+        }
+        $result = $result.'<email>email@journals.phil.muni.cz</email></author>';
+      }
+    }
+
+    return $result;
+  }
+
+  public function getTitles($article, $locales, $serial_languages) {
+    $main_title[] = $article->get('field_title_main')->getValue()[0];
+    $main_title_value = $main_title[0]['first'];
+
+    $variant_titles = $article->get('field_variant_title')->getValue();
+    $titles = array_merge($main_title, $variant_titles);
+    $result_xml = '';
+
+    $result = [];
+    foreach ($serial_languages as $lang) {
+      $result[$lang] = ''; 
+    }
+
+    foreach ($titles as $index => &$title) {
+      $lang = $locales[$title['second']];
+      $result[$lang] = $title['first'];
+    }
+
+    foreach ($serial_languages as $lang) {
+      if ($result[$lang] == '') {
+        $result[$lang] = $main_title_value;
+      }
+    }
+
+    foreach ($result as $lang => $title) {
+      $result_xml = $result_xml.'<title locale="'.$lang.'">'.$this->clean($title)."</title>";
+    }
+    return $result_xml;
+  }
+
+  public function getSerialLanguages($article) {
+    $issue_id = $article->get('field_member_of')->getValue()[0]['target_id'];
+    $issue = \Drupal\node\Entity\Node::load($issue_id);
+    if ($volume_id = $issue->get('field_member_of')->getValue()[0]['target_id']) {
+      $volume = \Drupal\node\Entity\Node::load($volume_id);
+      if ($serial_id = $volume->get('field_member_of')->getValue()[0]['target_id']) {
+        $serial = \Drupal\node\Entity\Node::load($serial_id);
+        $handle = $serial->get('field_handle')->getValue()[0]['value'];
+        $handle = trim($handle, 'digilib.');
+        $lang = '';
+
+        if (($csv = fopen("/var/www/html/drupal/web/modules/custom/digitalia_muni_xml/languages.csv", "r")) !== FALSE) {
+          while ($line = fgetcsv($csv)) {
+            if ($line[0] == $handle) {
+              $lang = $line[1];
+              break;
+            }
+          }
+          fclose($csv);
+          return explode('|', $lang);
+        }
+      }
+    }
+    return [];
+  }
+
+  public function clean($text) {
+    $characters = array('<', '>', '&');
+    $html_entities = array('&lt;', '&gt;', '&amp;');
+    return str_replace($characters, $html_entities, $text);
+  }
+}
---
+++ src/Plugin/views/style/DigitaliaXmlFeedViews.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Drupal\digitalia_muni_xml\Plugin\views\style;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\views\Plugin\views\style\StylePluginBase;
+use Drupal\xmlfeedviews\Plugin\views\style\XmlFeedViews;
+
+/**
+ * Default style plugin to render an OPML feed.
+ *
+ * @ingroup views_style_plugins
+ *
+ * @ViewsStyle(
+ *   id = "digitaliaxmlfeedviews",
+ *   title = @Translation("Digitalia XML Feed Views"),
+ *   help = @Translation("Generates an XML feed from a view."),
+ *   theme = "xmlfeedviews",
+ *   display_types = {"feed"}
+ * )
+ */
+class DigitaliaXmlFeedViews extends XmlFeedViews {
+
+  /**
+   * Get XML feed views header.
+   *
+   * @return string
+   *   The string containing the description with the tokens replaced.
+   */
+  public function getHeader() {
+    $header = $this->options['xmlfeedviews_head'];
+
+    // replace tokens
+    $issue = $this->tokenizeValue("{{ field_member_of }}", 0);
+    $volume = $this->tokenizeValue("{{ field_member_of_1 }}", 0);
+    $year = $this->tokenizeValue("{{ field_publication_year }}", 0);
+    $date = $this->tokenizeValue("{{ field_publication_date }}", 0);
+//    $date = $this->tokenizeValue("{{ published_at_1 }}", 0);
+    $issue_id = $this->tokenizeValue("{{ nid_1 }}", 0);
+    if (!$issue_id) {
+      return $header;
+    }
+
+    $published = $this->tokenizeValue("{{ status_1 }}", 0);
+    //replace with values
+    $header = str_replace("{{ field_member_of }}", $issue, $header);
+    $header = str_replace("{{ field_member_of_1 }}", $volume, $header);
+    $header = str_replace("{{ field_publication_year }}", $year, $header);
+    $header = str_replace("{{ field_publication_date }}", $date, $header);
+    $header = str_replace("{{ nid_1 }}", $issue_id, $header);
+    $header = str_replace("{{ status_1 }}", $published, $header);
+
+    // get issue
+    $issue = \Drupal\node\Entity\Node::load($issue_id);
+
+    // add sections
+    $sections = $this->getSections($issue);
+    $header = str_replace("{{ sections }}", $sections, $header);
+
+    // add cover
+    if (str_contains($header, '<covers>{{ cover }}</covers>')) {
+      $cover = $this->getCover($issue);
+      $header = str_replace("{{ cover }}", $cover, $header); 
+    }
+    //dpm(htmlspecialchars($header));
+    return $header;
+  }
+
+  public function getSerialLanguages($issue) {
+    if ($volume_id = $issue->get('field_member_of')->getValue()[0]['target_id']) {
+      $volume = \Drupal\node\Entity\Node::load($volume_id);
+      if ($serial_id = $volume->get('field_member_of')->getValue()[0]['target_id']) {
+        $serial = \Drupal\node\Entity\Node::load($serial_id);
+        $handle = $serial->get('field_handle')->getValue()[0]['value'];
+        $lang = '';
+        if (($csv = fopen("/var/www/html/drupal/web/modules/custom/digitalia_muni_xml/languages.csv", "r")) !== FALSE) {
+          while ($line = fgetcsv($csv)) {
+            if ($line[0] == $handle) {
+              $lang = $line[1];
+              break;
+            }
+          }
+          fclose($csv);
+          return explode('|', $lang);
+        }
+      }
+    }
+    return [];
+  }
+
+  public function getCover($issue) {
+    $media_array = \Drupal::entityTypeManager()->getStorage('media')->loadByProperties(['field_media_of' => $issue->id(), 'bundle' => 'image']);
+    if (empty($media_array)) {
+      return '';
+    }
+    $media = reset($media_array);
+    $fid = $media->get('field_media_image')->getValue()[0]['target_id'];
+    $file = \Drupal\file\Entity\File::load($fid);
+    $filename = $file->getFilename();
+    $url = 'http://digilib.phil.muni.cz'.$file->createFileUrl();
+    $type = pathinfo($url, PATHINFO_EXTENSION);
+    $data = file_get_contents($url);
+    if ($data === NULL) {
+      return '';
+    }
+    $result = '';
+    // $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+    $base64 = base64_encode($data);
+    //$base64 = 'base64';
+    $encoding = '<embed encoding="base64">'.$base64.'</embed>';
+    foreach ($this->getSerialLanguages($issue) as $lang) {
+      $name = '<cover_image>'.$filename.'</cover_image>';
+      $alt = '<cover_image_alt_text>Cover</cover_image_alt_text>';
+      $result = $result.'<cover locale="'.$lang.'">'.$name.$alt.$encoding.'</cover>';
+    }
+    return $result;
+  }
+
+  public function getSections($issue) {
+    $articles = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['field_member_of' => $issue->id(), 'status' => 1]);
+    $sections = [];
+    foreach ($articles as $article) {
+      if ($section_field = $article->get('field_section')->getValue()) {
+        $section = \Drupal\taxonomy\Entity\Term::load($section_field[0]['target_id']);
+        $name = $section->getName().'###'.$section_field[0]['target_id'];
+        $page = $article->get('field_pagination_from')->getValue()[0]['value'];
+        if (isset($sections[$name])) {
+          $sections[$name] = min($sections[$name], $page);
+        } else {
+          $sections[$name] = $page;
+        }
+      }
+    }
+    asort($sections);
+    $result = '<section ref="Hidden" seq="0" editor_restricted="1" meta_indexed="0" meta_reviewed="0" abstracts_not_required="1" hide_title="1" hide_author="0" abstract_word_count="0">
+      <id type="internal" advice="ignore">565</id>
+      <abbrev locale="cs_CZ">Hidden</abbrev>
+      <abbrev locale="en_US">Hidden</abbrev>
+      <title locale="cs_CZ">Skrytá sekce</title>
+      <title locale="en_US">Hidden section</title>
+    </section>';
+
+    $seq = 1;
+    $serial_languages = $this->getSerialLanguages($issue);
+    foreach ($sections as $s => $p) {
+      $name = explode('###', $s)[0];
+      $tid = explode('###', $s)[1];
+      $result = $result.'<section ref="'.$tid.'" seq="'.$seq.'"><id type="internal" advice="ignore">'.$tid.'</id>';
+      foreach ($serial_languages as $lang) {
+        $result = $result.'<abbrev locale="'.$lang.'">'.$name.'</abbrev>';
+      }
+      foreach ($serial_languages as $lang) {
+        $result = $result.'<title locale="'.$lang.'">'.$name.'</title>';
+      }
+      $result = $result.'</section>';
+
+      $seq += 1;
+    }
+    return $result;
+  }
+}
